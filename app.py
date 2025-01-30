60c5c5cfb1ebf77bf3c892306153bf1810549ece
Arzu <arzuh94a@gmail.com>
2025-01-30 19:21:56 +0400

    Removed notes

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-# Import necessary libraries
 from flask import Flask, render_template, request
 import requests
-from config import NEWS_API_KEY  # Your API key should be stored in config.py
+from config import NEWS_API_KEY 
 
-# Initialize Flask application
+
 app = Flask(__name__)
 
-# News API endpoint
+
 NEWS_API_URL = "https://newsapi.org/v2/everything"
 
-# Function to fetch news articles
+
 def get_news(keyword):
     params = {
         'q': keyword,
         'apiKey': NEWS_API_KEY,
-        'pageSize': 11  # Limit to 10 results
+        'pageSize': 11  
     }
     response = requests.get(NEWS_API_URL, params=params)
     data = response.json()
@@ -24,7 +23,7 @@ def get_news(keyword):
     else:
         return []
 
-# Home route
+
 @app.route("/", methods=["GET", "POST"])
 
 def home():
@@ -32,7 +31,7 @@ def home():
     if request.method == "POST":
         keyword = request.form.get("keyword")
         if keyword:
-            news_articles = get_news(keyword)  # Fetch articles based on the keyword
+            news_articles = get_news(keyword)  
     return render_template("index.html", articles=news_articles)
 
 if __name__ == "__main__":
